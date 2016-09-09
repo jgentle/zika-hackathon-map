5cccefb10907624257e290c5df9041608cc12d03
John Gentle <taoteg@gmail.com>
2016-09-09 16:05:28 -0500

    Working map with all basetiles wired in and layer controls. Now pulling out attrs from shapefile data for display.

---
+++ js/map.js
@@ -0,0 +1,73 @@
+console.log('map.js loaded.');
+
+//******************
+//*  Leaflet
+//*****************
+
+// GeoJson data sources.
+var countyData = '../data/TX_counties/tx_counties.geojson';
+var zikaIndexData = '../data/Zika_Index_Hack.geojson';
+
+// Layer data.
+var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.');
+var denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.');
+var aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.');
+var golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+
+var cities = L.layerGroup([littleton, denver, aurora, golden]);
+
+// Create map globals.
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+// Create default config objects.
+var defaultLat = 31.2;
+var defaultLong = -99; 
+var defaultZoom = 6;
+var defaultLocation = new L.LatLng(defaultLat, defaultLong);
+
+// Create the Leaflet map, set the default view and zoom, and add the basemap.
+var map = L.map('zikamap', {
+    center: defaultLocation,
+    zoom: defaultZoom,
+    layers: [Esri_WorldImagery]
+});
+
+// Control layers.
+var baseMaps = {
+    "OpenStreetMap_Mapnik": OpenStreetMap_Mapnik,
+    "OpenStreetMap_BlackAndWhite": OpenStreetMap_BlackAndWhite,
+    "Thunderforest_OpenCycleMap": Thunderforest_OpenCycleMap,
+    "Thunderforest_Transport": Thunderforest_Transport,
+    "Thunderforest_TransportDark": Thunderforest_TransportDark,
+    "Thunderforest_SpinalMap": Thunderforest_SpinalMap,
+    "Thunderforest_Landscape": Thunderforest_Landscape,
+    "Thunderforest_Pioneer": Thunderforest_Pioneer,
+    "OpenMapSurfer_Roads": OpenMapSurfer_Roads,
+    "OpenMapSurfer_Grayscale": OpenMapSurfer_Grayscale,
+    "Stamen_TonerLite": Stamen_TonerLite,
+    "Stamen_Watercolor": Stamen_Watercolor,
+    "Esri_WorldStreetMap": Esri_WorldStreetMap,
+    "Esri_WorldTopoMap": Esri_WorldTopoMap,
+    "Esri_WorldImagery": Esri_WorldImagery,
+    "Esri_OceanBasemap": Esri_OceanBasemap,
+    "Esri_NatGeoWorldMap": Esri_NatGeoWorldMap
+};
+
+var overlayMaps = {
+    "Cities": cities
+};
+
+// Add controls to map.
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+// Add data layers.
+var countyLayer = new L.GeoJSON.AJAX(countyData);
+console.log(countyLayer);     
+countyLayer.addTo(map);
+
+var zikaIndexLayer = new L.GeoJSON.AJAX(zikaIndexData);
+console.log(zikaIndexLayer);   
+zikaIndexLayer.addTo(map);
---
+++ js/maptile_providers.js
@@ -0,0 +1,114 @@
+console.log('maptile_providers.js loaded.');
+
+//******************
+//*  Maptiles
+//*****************
+
+// THUNDER FORREST
+
+var tf_apikey = 'adb04f7b20ed4448a70bc4044f425e81';
+
+var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 18,
+    apikey: tf_apikey
+});
+
+var Thunderforest_OpenCycleMap = L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}', {
+    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    apikey: tf_apikey
+});
+
+var Thunderforest_Transport = L.tileLayer('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey={apikey}', {
+    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 19,
+    apikey: tf_apikey
+});
+
+var Thunderforest_TransportDark = L.tileLayer('http://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey={apikey}', {
+    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 19,
+    apikey: tf_apikey
+});
+
+var Thunderforest_SpinalMap = L.tileLayer('http://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png?apikey={apikey}', {
+    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 11,
+    apikey: tf_apikey
+});
+
+var Thunderforest_Landscape = L.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}', {
+    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    apikey: tf_apikey
+});
+
+var Thunderforest_Pioneer = L.tileLayer('http://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey={apikey}', {
+    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    apikey: tf_apikey
+});
+
+var OpenMapSurfer_Roads = L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+    attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 20
+});
+
+var OpenMapSurfer_Grayscale = L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}', {
+    attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 19
+});
+
+// STAMEN
+
+var Stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+});
+
+var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+});
+
+var Stamen_Watercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 1,
+    maxZoom: 16,
+    ext: 'png'
+});
+
+// ESRI
+
+var Esri_WorldStreetMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+});
+
+var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+});
+
+var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var Esri_OceanBasemap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+    maxZoom: 13
+});
+
+var Esri_NatGeoWorldMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+    maxZoom: 16
+});
+
